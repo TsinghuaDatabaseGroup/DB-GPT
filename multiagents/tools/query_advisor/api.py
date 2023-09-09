1e4f018c97c054a1567eb3448b03b90c9449eb6d
Xuanhe Zhou <zhouxuan19@mails.tsinghua.edu.cn>
2023-09-09 14:02:33 +0800

    fix metric/rewrite/text-embed problems

--- multiagents/tools/query_advisor/api.py
+++ multiagents/tools/query_advisor/api.py
@@ -5,84 +5,10 @@ import jpype as jp
 import jpype.imports
 from multiagents.tools.metrics import postgresql_conf
 
+from multiagents.tools.query_advisor.rewrite_class import rule_is_valid
+
 # tranformation rules (45)
 # ['AGGREGATE_ANY_PULL_UP_CONSTANTS', 'AGGREGATE_EXPAND_DISTINCT_AGGREGATES', 'AGGREGATE_EXPAND_DISTINCT_AGGREGATES_TO_JOIN', 'AGGREGATE_JOIN_REMOVE', 'AGGREGATE_JOIN_TRANSPOSE_EXTENDED', 'AGGREGATE_UNION_TRANSPOSE', 'AGGREGATE_UNION_AGGREGATE', 'AGGREGATE_VALUES', 'AGGREGATE_PROJECT_MERGE', 'FILTER_INTO_JOIN', 'FILTER_MERGE', 'FILTER_AGGREGATE_TRANSPOSE', 'FILTER_PROJECT_TRANSPOSE', 'FILTER_TABLE_FUNCTION_TRANSPOSE', 'FILTER_SCAN', 'FILTER_CORRELATE', 'FILTER_SET_OP_TRANSPOSE', 'FILTER_REDUCE_EXPRESSIONS', 'JOIN_CONDITION_PUSH', 'JOIN_EXTRACT_FILTER', 'JOIN_PROJECT_BOTH_TRANSPOSE', 'JOIN_PROJECT_LEFT_TRANSPOSE', 'JOIN_PROJECT_RIGHT_TRANSPOSE', 'JOIN_REDUCE_EXPRESSIONS', 'JOIN_LEFT_UNION_TRANSPOSE', 'JOIN_RIGHT_UNION_TRANSPOSE', 'SEMI_JOIN_REMOVE', 'PROJECT_CALC_MERGE', 'PROJECT_CORRELATE_TRANSPOSE', 'PROJECT_REDUCE_EXPRESSIONS', 'PROJECT_SUB_QUERY_TO_CORRELATE', 'PROJECT_MERGE', 'PROJECT_REMOVE', 'PROJECT_TO_CALC', 'CALC_MERGE', 'CALC_REMOVE', 'SORT_UNION_TRANSPOSE', 'SORT_REMOVE', 'SORT_REMOVE_CONSTANT_KEYS', 'SORT_JOIN_TRANSPOSE', 'SORT_PROJECT_TRANSPOSE', 'UNION_MERGE', 'UNION_REMOVE', 'UNION_PULL_UP_CONSTANTS', 'UNION_TO_DISTINCT']
-
-class java_rewrite(object):
-    def __init__(self):
-        self.start_jvm()
-        pass
-
-    def start_jvm(self):
-        try:
-            # 如果已经启动jvm的虚拟环境则跳过；否则进入else，启动java的虚拟环境
-            if jpype.isJVMStarted():
-                return True
-            else:
-                base_dir = os.path.abspath(os.curdir)
-                local_lib_dir = os.path.join(base_dir+'/multiagents/tools/query_advisor/query_rewrite/', 'java_jar')
-                _ = os.popen(
-                    'mvn dependency:build-classpath -Dmdep.outputFile=classpath.txt').read()
-                classpath = open(
-                    os.path.join(
-                        base_dir,
-                        'classpath.txt'),
-                    'r').readline().split(':')
-                classpath.extend([os.path.join(local_lib_dir, jar)
-                                  for jar in os.listdir(local_lib_dir)])
-                jp.startJVM(jp.getDefaultJVMPath(), classpath=classpath)
-                print("系统启动java的jvm虚拟环境成功")
-                return True
-        except Exception as e:
-            print("系统启动java的jvm虚拟环境出现错误,错误原因:" + str(e))
-            return False
-
-
-    def stop_jvm(self):
-        try:
-            if jpype.isJVMStarted():
-                jpype.shutdownJVM()
-                print("关闭java的jvm虚拟环境成功")
-        except Exception as e:
-            print("关闭java的jvm虚拟环境出错,错误原因:" + str(e))
-            return False
-
-    def single_rule_rewrite(self, query, rule):
-        try:
-            if self.start_jvm():
-                # 如果遇到签名问题，执行``` zip -d java_parser.jar 'META-INF/.SF' 'META-INF/.RSA' 'META-INF/*SF' ```
-                SingleRewrite = jp.JClass("SingleRewrite")
-                # String rule, String sql, String host, String port, String user, String passwd, String dbType/mysql/postgresql
-                res = SingleRewrite.singleRule(rule, query, postgresql_conf.get('host'), str(postgresql_conf.get('port')), postgresql_conf.get('user'), postgresql_conf.get('password'), postgresql_conf.get('dbname'), "postgresql")
-                return res
-        except Exception as e:
-            print("JAVA调用singleRule方法出错,错误原因:" + str(e))
-            return {
-                "status": False,
-                "message": str(e)
-            }
-
-JavaRewrite = java_rewrite()
-
-# 判断改写后语句是否有效
-def rule_is_valid(rule, query):
-    res = JavaRewrite.single_rule_rewrite(query, rule)
-    data = res.get('data')
-
-    if res is None or not res.get('status'):
-        return f"Failed to optimize the query. error msg is {res.get('message')}"
-
-    dbargs = DBArgs("postgresql", config=postgresql_conf)  # todo assign database name
-    db = Database(dbargs, timeout=-1)
-    new_query = data.get('rewritten_sql')
-    new_query_plan = db.pgsql_query_plan(new_query)
-    if new_query_plan is None:
-        text_output = f"Failed to optimize the query. The new query is still {query}"
-    else:
-        total_cost, operators = db.query_plan_statistics(new_query_plan)
-        text_output = f"New query is {new_query}\nNew query plan statistics: total cost is {total_cost}, and the operators are {operators}"
-    return text_output
-
 
 def AGGREGATE_ANY_PULL_UP_CONSTANTS_rule(query):
     return rule_is_valid("AGGREGATE_ANY_PULL_UP_CONSTANTS", query)
