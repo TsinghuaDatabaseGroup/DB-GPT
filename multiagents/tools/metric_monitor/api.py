77f2995e12236eace5bd986f1b5182384c2e0653
Xuanhe Zhou <zhouxuan19@mails.tsinghua.edu.cn>
2023-10-07 06:38:17 +0800

    add text2img

--- multiagents/tools/metric_monitor/api.py
+++ multiagents/tools/metric_monitor/api.py
@@ -6,7 +6,7 @@ from multiagents.tools.metrics import diag_start_time, diag_end_time
 import pdb
 from multiagents.tools.metrics import slow_queries, workload_statistics
 from multiagents.tools.metrics import knowledge_matcher
-
+from utils.markdown_format import generate_prometheus_chart_content
 from multiagents.tools.index_advisor.api import optimize_index_selection
 
 def obtain_start_and_end_time_of_anomaly(input: str = 'json dict string'):
@@ -36,15 +36,21 @@ def whether_is_abnormal_metric(
     else:
         raise Exception("No metric values found for the given time range")
 
+    # draw the metric chart
+    chart_metric_values = [[i, str(value)] for i, value in enumerate(metric_values)]
+    chart_content = generate_prometheus_chart_content(metric_name, chart_metric_values, x_label_format="%H:%M", size=(400, 225))
+    with open(f"./alert_results/test/{metric_name}.html", "w") as f:
+        f.write(chart_content)
+
     is_abnormal = detect_anomalies(
         np.array([float(value) for _, value in metric_values]))
 
     if is_abnormal:
         print(f"{metric_name} is abnormal")
-        return f"The metric {metric_name} is abnormal \n {prometheus_metrics[metric_name]}"
+        return f"The metric {metric_name} is abnormal \n " + f"[chart] ./alert_results/test/{metric_name}.html"
     else:
         print(f"{metric_name} is normal")
-        return f"The metric {metric_name} is normal \n {prometheus_metrics[metric_name]}"
+        return f"The metric {metric_name} is normal \n " + f"[chart] ./alert_results/test/{metric_name}.html"
     
 
 def match_diagnose_knowledge(
@@ -101,6 +107,17 @@ def match_diagnose_knowledge(
     
     detailed_abnormal_metrics = top5_abnormal_metrics
 
+    for i,metric_name in enumerate(top5_abnormal_metrics):
+        metric_values = top5_abnormal_metrics[metric_name]
+
+        # draw the metric chart
+        chart_metric_values = [[i, str(value)] for i, value in enumerate(metric_values)]
+        chart_content = generate_prometheus_chart_content(metric_name, chart_metric_values, x_label_format="%H:%M", size=(400, 225))
+        with open(f"./alert_results/test/{metric_name}.html", "w") as f:
+            f.write(chart_content)
+
+        detailed_abnormal_metrics = detailed_abnormal_metrics + f"{i+1}. {metric_name} contains abnormal patterns:\n [chart] ./alert_results/test/{metric_name}.html \n"
+        
     if metric_prefix == "network":
         
         return """The {} relevant metric values from Prometheus are: 
