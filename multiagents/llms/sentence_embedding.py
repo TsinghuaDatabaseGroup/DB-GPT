fa14bbef91f1284b4f8f0e3c7527538dee675b91
Xuanhe Zhou <zhouxuan19@mails.tsinghua.edu.cn>
2023-12-07 15:24:22 +0800

    [add] dynamic tool matching

--- multiagents/llms/sentence_embedding.py
+++ multiagents/llms/sentence_embedding.py
@@ -20,49 +20,49 @@ def sentence_embedding(sentence: str, model: str = "text-embedding-ada-002"):
     
     api_key = os.environ.get("OPENAI_API_KEY")
 
-    # client = OpenAI( api_key=api_key)
-    # timeout=10
-    # ok = 0
-    # while timeout>0:
-    #     try:
-    #         response = client.embeddings.create(input=[sentence], model=model)
-    #         ok = 1
-    #         break
-    #     except Exception as e:
-    #         time.sleep(.01)
-    #         timeout -= 1
-
-    # if ok == 0:
-    #     raise Exception("Failed to get response from API!")
-
-    # embedding = response.data[0].embedding
-    # return embedding
-
-
-    payload = {
-        "input": [sentence],
-        "model": model
-    }
-    url = "https://api.aiaiapi.com/v1/embeddings"
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + api_key
-    }
-
+    client = OpenAI( api_key=api_key)
     timeout=10
     ok = 0
     while timeout>0:
         try:
-            response = requests.post(url, json=payload, headers=headers)
+            response = client.embeddings.create(input=[sentence], model=model)
             ok = 1
             break
         except Exception as e:
             time.sleep(.01)
             timeout -= 1
-    
-    if ok == 0:
-        raise Exception("Failed to get response from openai API!")
 
-    embedding = json.loads(response.text)['data'][0]['embedding']
-    
+    if ok == 0:
+        raise Exception("Failed to get response from API!")
+
+    embedding = response.data[0].embedding
     return embedding
+
+
+    # payload = {
+    #     "input": [sentence],
+    #     "model": model
+    # }
+    # url = "https://api.aiaiapi.com/v1/embeddings"
+    # headers = {
+    #     "Content-Type": "application/json",
+    #     "Authorization": "Bearer " + api_key
+    # }
+
+    # timeout=10
+    # ok = 0
+    # while timeout>0:
+    #     try:
+    #         response = requests.post(url, json=payload, headers=headers)
+    #         ok = 1
+    #         break
+    #     except Exception as e:
+    #         time.sleep(.01)
+    #         timeout -= 1
+    
+    # if ok == 0:
+    #     raise Exception("Failed to get response from openai API!")
+
+    # embedding = json.loads(response.text)['data'][0]['embedding']
+    
+    # return embedding
