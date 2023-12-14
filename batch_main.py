9b3ed81b02c0da8dd61eb57fdf4c137a3cb445ef
Xuanhe Zhou <zhouxuan19@mails.tsinghua.edu.cn>
2023-12-14 23:02:32 +0800

    [update] batch_main.py

--- batch_main.py
+++ batch_main.py
@@ -6,6 +6,7 @@ import jsonlines
 import time
 import os
 import asyncio
+from pathlib import Path
 
 from multiagents.multiagents import MultiAgents
 from multiagents.tools.metrics import database_server_conf, db
@@ -16,7 +17,6 @@ from multiagents.utils.server import obtain_slow_queries
 async def main(process_id):
     global anomaly_jsons, result_jsons
 
-    # copy a new args
     new_args = args
 
     if process_id + 1 == process_num:
@@ -27,9 +27,10 @@ async def main(process_id):
         diag_ids = list(anomaly_jsons.keys())[process_id*split_size: (process_id+1)*split_size]
 
     for diag_id in diag_ids:
-        if diag_id not in finished_diag_ids:
+        record_name = reports_log_dir_name + "/diag_"+diag_id+".jsonl"
+        if not os.path.exists(record_name) and diag_id not in finished_diag_ids:
             content = anomaly_jsons[diag_id]
-
+            
             # ============================= workload info =============================
             new_args.diag_id = diag_id
 
@@ -66,17 +67,29 @@ async def main(process_id):
 
             multi_agents = MultiAgents.from_task(new_args.agent_conf_name, new_args)
             report, records = await multi_agents.run(new_args)
-            
+
+            # ================== vanilla model ==================
+            # diag, labels = await multi_agents.run(new_args)
+            # result_jsons[diag_id]["diag_"+method] = diag
+            # result_jsons[diag_id]["label_"+method] = labels
+
+            # out_file.write(str(diag_id))
+            # out_file.write(result_jsons[diag_id])
+            # out_file.write('==========================')
+            # print(str(diag_id), '========== ok ================')
+            # ===================================================
+
             # detailed log
             with open(reports_log_dir_name + "/diag_"+diag_id+".jsonl", "w") as f:
                 json.dump(report, f, indent=4)
 
-            with open(reports_log_dir_name + f"/{str(new_args.start_at_seconds)}.jsonl", "w") as f:
+            with open(log_dir_name + f"/{str(new_args.start_at_seconds)}.jsonl", "w") as f:
                 json.dump(records, f, indent=4)
 
             # result logs
             result_jsons[diag_id]["diag_"+method] = report["root cause"]
             result_jsons[diag_id]["solution_"+method] = report["solutions"]
+            result_jsons[diag_id]["label_"+method] = report["labels"]
 
             out_file.write(str(diag_id))
             out_file.write(result_jsons[diag_id])
@@ -88,13 +101,12 @@ def wrapper(i):
 
 # read from the anomalies with alerts. for each anomaly, 
 process_num = 4
-output_answer_file = "batch_testing_answers.jsonl"
-result_log_prefix = "./alert_results/logs/diagnosis_results/"
-log_dir_name = result_log_prefix + "2023-11-09-12-10-11"
+result_log_prefix = "./alert_results/logs/"
+log_dir_name = result_log_prefix + "batch_logs"
 reports_log_dir_name = log_dir_name + "/reports"
 
 method = "d_bot_gpt4"
-finished_diag_ids = [] #["6","9","10"] # finished_diag_ids = ['0', '14', '2', '25', '30', '36', '41', '49', '54', '6', '1', '15', '20', '26', '31', '37', '42', '5', '55', '60', '10', '16', '21', '27', '32', '38', '45', '50', '56', '61', '11', '17', '22', '28', '33', '39', '46', '51', '57', '7', '12', '18', '23', '29', '34', '4', '47', '52', '58', '8', '13', '19', '24', '3', '35', '40', '48', '53', '59', '9']
+finished_diag_ids = [] #["45","30","15", "0", "46", "31", "1"] #["2", "6", "8", "9", "10", "12"] #["6","9","10"] # finished_diag_ids = ['0', '14', '2', '25', '30', '36', '41', '49', '54', '6', '1', '15', '20', '26', '31', '37', '42', '5', '55', '60', '10', '16', '21', '27', '32', '38', '45', '50', '56', '61', '11', '17', '22', '28', '33', '39', '46', '51', '57', '7', '12', '18', '23', '29', '34', '4', '47', '52', '58', '8', '13', '19', '24', '3', '35', '40', '48', '53', '59', '9']
 
 with open(BATCH_ANOMALY_FILE_NAME, "r") as f:
     anomaly_jsons = json.load(f)
@@ -109,6 +121,7 @@ for anomaly_id in anomaly_jsons:
             alert_names.append(alert['alerts'][0]['labels']['alertname'])
     result_jsons[anomaly_id] = {"labels": anomaly["labels"], 
                                 "alerts": alert_names,
+                                "label_"+method: "",
                                 "diag_"+method: "",
                                 "solution_"+method: ""}
 
@@ -117,13 +130,11 @@ out_file = jsonlines.open(log_dir_name + "/overall_diag_results.json", "a")
 out_file._flush = True
 
 if __name__=='__main__':
-
-    if not os.path.exists(log_dir_name):
-        os.makedirs(log_dir_name)
-
+    
     if not os.path.exists(reports_log_dir_name):
-        os.makedirs(reports_log_dir_name)
+        Path(reports_log_dir_name).mkdir(parents=True, exist_ok=True)
     
     with mp.Pool(processes=process_num) as pool:
         pool.map(wrapper, range(process_num))
+
     pool.terminate()
