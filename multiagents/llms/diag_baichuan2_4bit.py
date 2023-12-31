3e94843ae56e3db614206723054bc424220853fe
Zhaoyan Sun <53212907+curtis-sun@users.noreply.github.com>
2023-12-31 15:17:13 +0800

    locallized llms using transformers

--- multiagents/llms/diag_baichuan2_4bit.py
+++ multiagents/llms/diag_baichuan2_4bit.py
@@ -2,10 +2,10 @@ from multiagents.llms.local_chat_model import LocalChatModel
 from . import llm_registry
 import sys
 sys.path.append("../../../")
-from localized_llms.inference import baichuan2_inference
+from localized_llms.inference import baichuan2_4bit_inference
 
-@llm_registry.register("diag-baichuan2")
+@llm_registry.register("diag-baichuan2-4bit")
 class DiagBaichuan2_4bitChat(LocalChatModel):
     def __init__(self, max_retry: int = 100, **kwargs):
         super().__init__(max_retry=max_retry, **kwargs)
-        self.inference = baichuan2_inference
+        self.inference = baichuan2_4bit_inference
