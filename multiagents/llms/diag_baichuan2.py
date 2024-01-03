c7697fd5499fce55c61dcdc7536def5a6571bf85
Zhaoyan Sun <53212907+curtis-sun@users.noreply.github.com>
2024-01-03 18:12:02 +0800

    update llms

--- multiagents/llms/diag_baichuan2.py
+++ multiagents/llms/diag_baichuan2.py
@@ -5,7 +5,7 @@ sys.path.append("../../../")
 from localized_llms.inference import baichuan2_inference
 
 @llm_registry.register("diag-baichuan2")
-class DiagBaichuan2_4bitChat(LocalChatModel):
+class DiagBaichuan2Chat(LocalChatModel):
     def __init__(self, max_retry: int = 100, **kwargs):
         super().__init__(max_retry=max_retry, **kwargs)
         self.inference = baichuan2_inference
