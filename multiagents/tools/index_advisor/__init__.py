aac773bf890631f714374013fe061e88fa9b1de3
earthwuyang <2932818869@qq.com>
2024-03-24 15:58:15 +0800

    update index advising tools

--- multiagents/tools/index_advisor/__init__.py
+++ multiagents/tools/index_advisor/__init__.py
@@ -1,5 +1,5 @@
 
-from .index_selection.selection_utils.postgres_dbms import PostgresDatabaseConnector
-from .index_selection.selection_utils.workload import Workload
-from .index_selection.selection_utils import selec_com
-from .configs import INDEX_SELECTION_ALGORITHMS, get_index_result
+# from .index_selection.selection_utils.postgres_dbms import PostgresDatabaseConnector
+# from .index_selection.selection_utils.workload import Workload
+# from .index_selection.selection_utils import selec_com
+# from .configs import INDEX_SELECTION_ALGORITHMS, get_index_result
--- multiagents/tools/index_advisor/index_selection/selection_utils/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Project: index_test
-# @Module: __init__
-# @Author: Wei Zhou
-# @Time: 2022/6/12 16:52
