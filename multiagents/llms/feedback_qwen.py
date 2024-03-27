24bfb9036af68426271c2bf08a653b00cb02c878
tzy <461805682@qq.com>
2024-03-27 14:11:06 +0800

    tiny fix

--- multiagents/llms/feedback_qwen.py
+++ multiagents/llms/feedback_qwen.py
@@ -3,8 +3,8 @@ from .qwen_vllm import QwenVllmChat
 from .doc_kb import DocKnowledgeBase
 from .feedback_openai import get_cur_task, get_time
 import copy
-from multiagents.utils.interact import user_input, add_display_message, add_edit_message, add_feedback_message, \
-    finish_feedback_message, finish_select_edit_message, close_feedback_needInput
+from multiagents.utils.interact import (user_input, add_display_message, add_edit_message, add_feedback_message,
+                                        finish_feedback_message, finish_select_edit_message)
 
 # EXTRACT_RULE_PROMPT_zh = """我对你之前的回复不太满意。现在给你一个我更喜欢的回复，请比较它们的差异，并总结成规则，以便你以后能给出更好的回复。
 # 要求如下：
@@ -60,7 +60,7 @@ class FeedbackQwenChat(QwenVllmChat):
         feedback_placeholder = "请输入你对D-Bot回复的反馈（例如：你必须按xxx格式回复；关于xxx，你应该提供更多细节）。"
         add_feedback_message(cur_task, role, feedback_placeholder, res['content'], get_time())
         feedback = user_input(feedback_placeholder + '\n')
-        close_feedback_needInput()
+        finish_feedback_message()
         if feedback.strip() == '' or feedback.lower() == 'continue' or not self.judge_feedback(feedback):
             empty_feedback_placeholder = '没有/无效反馈，诊断继续。'
             add_display_message(cur_task, role, empty_feedback_placeholder, get_time())
@@ -75,7 +75,7 @@ class FeedbackQwenChat(QwenVllmChat):
             eval_placeholder = "你对改进的回答满意吗？请只回答“是”或“否”。"
             add_feedback_message(cur_task, role, eval_placeholder, refined_reply, get_time())
             eval_ = user_input(eval_placeholder + '\n')
-            close_feedback_needInput()
+            finish_feedback_message()
             if "否" in eval_:
                 refine_placeholder = '请详细输入你认为正确的回答。'
                 add_edit_message(cur_task, role, refine_placeholder, res['content'], get_time())
