48b71c2d8da8f9f85c5f72f0ba36bfc3ef5aab89
建明 武 <wjmwjmwb@yeah.net>
2024-05-21 09:48:24 +0800

    fix path bug

--- prometheus_service_docker/prometheus_abnormal_metric.py
+++ prometheus_service_docker/prometheus_abnormal_metric.py
@@ -15,7 +15,7 @@ PROMETHEUS_CONFIG = {
 }
 
 prometheus_metrics = read_prometheus_metrics_yaml(
-    config_path='./prometheus_service/prometheus_metrics.yaml',
+    config_path='./prometheus_service_docker/prometheus_metrics.yaml',
     node_exporter_instance=PROMETHEUS_CONFIG.get('node_exporter_instance'),
     postgresql_exporter_instance=PROMETHEUS_CONFIG.get(
         'postgresql_exporter_instance'))
